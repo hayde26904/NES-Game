8b174b3ada9f6582562549fff4fcbf6788fe4de0
hayde26904 <hayde26904@live.ytech.edu>
2024-04-03 22:05:07 -0400

    took highly detailed notes on code I got from watching a tutorial.

--- game.s
+++ game.s
@@ -1,153 +1,132 @@
 .segment "HEADER"
-  ; .byte "NES", $1A      ; iNES header identifier
-  .byte $4E, $45, $53, $1A
-  .byte 2               ; 2x 16KB PRG code
-  .byte 1               ; 1x  8KB CHR data
-  .byte $01, $00        ; mapper 0, vertical mirroring
 
-.segment "VECTORS"
-  ;; When an NMI happens (once per frame if enabled) the label nmi:
-  .addr nmi
-  ;; When the processor first turns on or is reset, it will jump to the label reset:
-  .addr reset
-  ;; External interrupt IRQ (unused)
-  .addr 0
+  .byte "NES"
+  .byte $1A
+  .byte $02 ; 2 16kb PRG ROM chips
+  .byte $01 ; 1 8kb CHR ROM chip
+  .byte %00000000 ; mapper and mirroring
+  .byte $00
+  .byte $00
+  .byte $00
+  .byte $00
+  .byte $00, $00, $00, $00, $00 ; filler bytes
 
-; "nes" linker config requires a STARTUP section, even if it's empty
+.segment "ZEROPAGE" ; LSB 0 - FF
+
 .segment "STARTUP"
 
-; Main code segment for the program
-.segment "CODE"
+  Reset:
+    sei ; Disables all interrupts so this code is not interrupted, my son.
+    cld ; disables decimal mode because NES doesn't support decimals, my child.
 
-reset:
-  sei		; disable IRQs
-  cld		; disable decimal mode
-  ldx #$40
-  stx $4017	; disable APU frame IRQ
-  ldx #$ff 	; Set up stack
-  txs		;  .
-  inx		; now X = 0
-  stx $2000	; disable NMI
-  stx $2001 	; disable rendering
-  stx $4010 	; disable DMC IRQs
+    ; Disable sound IRQ to make sure values aren't interpreted as sound data and random sounds don't play when we load the program.
+    ldx #$40
+    stx $4017 ; APU Frame Counter
 
-;; first wait for vblank to make sure PPU is ready
-vblankwait1:
-  bit $2002
-  bpl vblankwait1
+    ; Initialize the stack register. Shhh, be nice to it. We wouldn't want it to overflow...
+    ldx #$FF ; We load FF because the stack register decrements with each addition to stack instead of incrementing like you'd expect
+    txs ; Transfer X to Stack Register
 
-clear_memory:
-  lda #$00
-  sta $0000, x
-  sta $0100, x
-  sta $0200, x
-  sta $0300, x
-  sta $0400, x
-  sta $0500, x
-  sta $0600, x
-  sta $0700, x
-  inx
-  bne clear_memory
+    inx ;  #$FF + 1 = #$00
 
-;; second wait for vblank, PPU is ready after this
-vblankwait2:
-  bit $2002
-  bpl vblankwait2
+    ; Zero out PPU registers se we don't see random graphics when we load the program
+    stx $2000 ; PPU control address
+    stx $2001 ; PPU mask address. Stores whether background and sprites are enabled and some other options like that, fam.
 
-main:
-load_palettes:
-  lda $2002
-  lda #$3f
-  sta $2006
-  lda #$00
-  sta $2006
-  ldx #$00
-@loop:
-  lda palettes, x
-  sta $2007
-  inx
-  cpx #$20
-  bne @loop
+    stx $4010 ; This also disables sounds and shiz.
+  
+    ;We need to check that the console has attempted to draw its first V-Blank. This tells us that it is ready to rumble!
+    ; BIT gets the 7th bit from an address and evaluates that value to either branch or continue. The 7th bit signifies whether the value is signed or not
+:
+    bit $2002
+    bpl :- ; branch to previous anonymous label, if positive. if we are in a vblank, $2002 will be signed negative and the 7th bit will be 1. Otherwise, it is 0 and it is positive.
 
-enable_rendering:
-  lda #%10000000	; Enable NMI
-  sta $2000
-  lda #%00010000	; Enable Sprites
-  sta $2001
+    txa ; transfers X reg to A reg. The value is currently 0
 
-forever:
-  jmp forever
+  CLEARMEM: ; This is basically a low level for loop that clears out the NES memory.
+    sta $0000, X ; X is the offset, so its value will be added to the address to clear out every column of memory at the address
+    sta $0100, X
+    sta $0300, X ; Notice how we don't clear $0200 because we usin that for sprite data and shiz
+    sta $0400, X
+    sta $0500, X
+    sta $0600, X
+    sta $0700, X
 
-nmi:
-  ldx #$00 	; Set SPR-RAM address to 0
-  stx $2003
-@loop:	lda hello, x 	; Load the hello message into SPR-RAM
-  sta $2004
-  inx
-  cpx #$1c
-  bne @loop
-  rti
+    ;initializing our Sprite data address
+    lda #$FF
+    sta $0200, X ; stores FF into all memory columns of address $0200
+    lda #$00 ; set back to 00 so we can go back to for loopin like nothin ever happened
 
-hello:
-  .byte $00, $00, $00, $00 	; Why do I need these here?
-  .byte $00, $00, $00, $00
-  .byte $6c, $00, $00, $6c
-  .byte $6c, $01, $00, $76
-  .byte $6c, $02, $00, $80
-  .byte $6c, $03, $00, $8A
-  .byte $6c, $03, $00, $94
+    inx
+    bne CLEARMEM ; we loop back until X equals A, or 0 in this case
 
-palettes:
-  ; Background Palette
-  .byte $0f, $00, $00, $00
-  .byte $0f, $00, $00, $00
-  .byte $0f, $00, $00, $00
-  .byte $0f, $00, $00, $00
+  : ; Waiting for a second Vblank to make extra extra sure that the PPU is ready for us to grace it with our fabulous art.
+    bit $2002 ; branch to previous anonymous label, if positive.
+    bpl :-
 
-  ; Sprite Palette
-  .byte $0f, $20, $00, $00
-  .byte $0f, $00, $00, $00
-  .byte $0f, $00, $00, $00
-  .byte $0f, $00, $00, $00
+    lda #$02 ; The HIbyte of our PPU data address.
+    sta $4014 ; OAM DMA Register. Tells the OAM where our sprite data is or some shiz not entirely sure.
+    nop ; PPU needs some time, so give it some time with the new No Operation instruction!
 
-; Character memory
+    ; tell PPU that we want to write to address $3F00. $3F00 is the starting address for palette information in PPU memory
+    lda #$3F
+    sta $2006 ; this is the PPU address register. It allows us to communicate with the PPU from the CPU.
+    lda #$00
+    sta $2006
+
+    ldx #$00
+
+  LoadPalettes: ; for loop that loops through palette data and places it in ppu memory
+    lda PaletteData, X
+    sta $2007 ; while $2006 is for the read/write location, $2007 is for the actual read write data. They are like 2 peas in a pod!
+    ; the ppu also automatically increments the address we are writing at each time we write a byte. This is very nifty indeed. Finally, the machine does something for me.
+    inx
+    cpx #$20 ; 20 in hex is 32 in decimal. We are loading 32 bytes of palette data in this case.
+    bne LoadPalettes
+
+    ldx #$00
+
+  LoadSprites: ; for loop that loops through sprite data and places it in our previously set sprite data address
+    lda SpriteData, X
+    sta $0200, X ; Stores the sprite byte offsetted by X into our previously set sprite address also offsetted by X
+    inx
+    cpx #$20
+    bne LoadSprites
+
+    cli ; Turn interrupts back on (because we had them off before for initialization purposes)
+
+    lda #%10010000 ; tells ppu that we want to be interrupted for NMI and that we want to use tileset 2 for background tiles instead of tileset1
+    sta $2000 ; store in ppu control reg
+    lda #%00011110 ; tells ppu that we want to enable rendering for first leftmost 8px for sprites and background and to enable rendering of sprites and background in general
+    sta $2001
+
+
+  Loop: ; looping so we don't run the NMI code when there isn't an NMI, pluh. THIS IS NOT A GAMELOOP!!!!!
+    jmp Loop
+
+  NMI: ; occurs in between the drawing of frames.
+    lda #$02
+    sta $4014 ; tell ppu where to find sprite data (we have to do this every frame)
+    rti ;  RTS but for interrupts because it needs to be a different thing for some reason.
+  
+  PaletteData:
+    .byte $22,$29,$1A,$0F,$22,$36,$17,$0f,$22,$30,$21,$0f,$22,$27,$17,$0F  ;background palette data
+    .byte $22,$16,$27,$18,$22,$1A,$30,$27,$22,$16,$30,$27,$22,$0F,$36,$17  ;sprite palette data
+
+  SpriteData:
+    .byte $08, $00, $00, $08 ; Ypos, Tile, Attributes, Xpos
+    .byte $08, $01, $00, $10
+    .byte $10, $02, $00, $08
+    .byte $10, $03, $00, $10
+    .byte $18, $04, $00, $08
+    .byte $18, $05, $00, $10
+    .byte $20, $06, $00, $08
+    .byte $20, $07, $00, $10
+
+.segment "VECTORS" ; for interrupt handlers and shiz
+  .word NMI
+  .word Reset
+  ;
+
 .segment "CHARS"
-  .byte %11000011	; H (00)
-  .byte %11000011
-  .byte %11000011
-  .byte %11111111
-  .byte %11111111
-  .byte %11000011
-  .byte %11000011
-  .byte %11000011
-  .byte $00, $00, $00, $00, $00, $00, $00, $00
-
-  .byte %11111111	; E (01)
-  .byte %11111111
-  .byte %11000000
-  .byte %11111100
-  .byte %11111100
-  .byte %11000000
-  .byte %11111111
-  .byte %11111111
-  .byte $00, $00, $00, $00, $00, $00, $00, $00
-
-  .byte %11000000	; L (02)
-  .byte %11000000
-  .byte %11000000
-  .byte %11000000
-  .byte %11000000
-  .byte %11000000
-  .byte %11111111
-  .byte %11111111
-  .byte $00, $00, $00, $00, $00, $00, $00, $00
-
-  .byte %01111110	; O (03)
-  .byte %11100111
-  .byte %11000011
-  .byte %11000011
-  .byte %11000011
-  .byte %11000011
-  .byte %11100111
-  .byte %01111110
-  .byte $00, $00, $00, $00, $00, $00, $00, $00
+  .incbin "mario.chr"
