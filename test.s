bf63913fd899c66d8f0f301f57d60abdbfc80ddb
hayde26904 <haydenfrobenius750@gmail.com>
2024-10-24 23:46:22 -0400

    block

--- test.s
+++ test.s
@@ -30,38 +30,61 @@
     ldx #$FF
     txs
 
+  lda #%10010000
+  sta PPU_CTRL
+  lda #%00011110
+  sta PPU_MASK
+
   waitVblank:
     bit $2002
     bpl waitVblank
-
-    lda #%10000000
-    sta $2000
-    lda #%00011110
-    sta $2001
 
   ; todo
   ; look into using structs to store all info about a sprite.
 
   loadMarioPalette:
 
-    lda #$3F
-    sta $2006
-    lda #$10
-    sta $2006
+    lda PPU_STATUS
 
     lda #$3F
-    sta $2007
+    sta PPU_ADDR
+    lda #$10
+    sta PPU_ADDR
+
+    lda #$22
+    sta PPU_DATA
 
     lda #$16
-    sta $2007
+    sta PPU_DATA
 
     lda #$27
-    sta $2007
+    sta PPU_DATA
 
     lda #$18
-    sta $2007
+    sta PPU_DATA
 
-  lodaMarioSprite:
+  loadBlockPalette:
+
+    lda PPU_STATUS
+
+    lda #$3F
+    sta PPU_ADDR
+    lda #$00
+    sta PPU_ADDR
+
+    lda #$22
+    sta PPU_DATA
+
+    lda #$27
+    sta PPU_DATA
+
+    lda #$17
+    sta PPU_DATA
+
+    lda #$18
+    sta PPU_DATA
+
+  loadMarioSprite:
     lda #$36
     sta $0201
     lda #$37
@@ -70,6 +93,37 @@
     sta $0209
     lda #$39
     sta $020D
+
+  loadBlockTiles: ;53 54 55 56
+
+    lda PPU_STATUS
+
+    lda #$20
+    sta PPU_ADDR
+    lda #$45
+    sta PPU_ADDR
+
+    lda #$53
+    sta PPU_DATA
+    lda #$54
+    sta PPU_DATA
+
+    lda PPU_STATUS
+
+    lda #$20
+    sta PPU_ADDR
+    lda #$65
+    sta PPU_ADDR
+
+    lda #$55
+    sta PPU_DATA
+    lda #$56
+    sta PPU_DATA
+
+    ;fix scroll for some reason
+    lda #0
+    sta $2005
+    sta $2005
 
   lda #$00
   sta OAM_ADDR
